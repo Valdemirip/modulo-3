23674b747a56c0724e560c0746c2138c0092c062
valdemirip <valdemirip@hotmail.com>
2026-01-19 21:10:35 -0300

    Post3

--- dados.php
+++ dados.php
@@ -2,8 +2,41 @@
 
 if (!empty($_POST['login'] && !empty($_POST['password']))) {
 
-$login = $_POST['login'];
-$password = $_POST['password'];
+$login = htmlspecialchars($_POST['login']);
+$password = htmlspecialchars($_POST['password']);
+$mensagem = null;
+
+if ($login == "admin" && $password == "12345") {
+    $mensagem =  "Login realizado com sucesso!";
+} else {
+    $mensagem = "Login ou senha incorretos!";
 }
-echo "Seu login é: $login, e a senha é: $password";
+
+} else {
+    $mensagem =  "Por favor, preencha todos os campos!";
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "dados.php" method="POST">
+        <input type = "text" name="login" placeholder = "Digite o Login">
+        <br>
+        <br>
+        <input type = "password" name="password" placeholder = "Digite a senha">
+        
+        <input type = "submit" value="enviar">
+    </form>
+<?php
+    echo $mensagem
+?>
+
+</body>
+</html>
